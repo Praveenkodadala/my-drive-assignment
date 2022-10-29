fb130439b64371eb96f38d422622b2a5d310aa27
Praveenkodadala <55790663+Praveenkodadala@users.noreply.github.com>
2022-10-29 20:06:54 +0530

    user data and token data saving in localstorage is done

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -62,8 +62,9 @@ export class LoginComponent implements OnInit {
           //console.log(res.token);
           localStorage.setItem('auth-token', res.token);
           this.httpService.token = res.token;
-          this.httpService.userData = res.userId;
-
+        
+          this.httpService.userData = res.userData
+          this.httpService.setItem('userData', JSON.stringify(res.userData))
           this.router.navigate(['user']);
 
        
@@ -89,60 +90,6 @@ export class LoginComponent implements OnInit {
 
 
  
-  getUserData() {
-    const request = {
-      method: 'GET',
-      action_url: 'user/get_user_profile',
-    }
-    this.httpService.doHttp(request)?.subscribe(
-      (res: any) => {
-        if (res.status) {
-          console.log("get user data at login cmpnt", res.data)
-          this.userInfo = true
-          this.userData = res.data
-          this.httpService.userData = this.userData
-          this.httpService.setItem('userData', JSON.stringify(this.userData))
 
-
-          if (this.userData.role.length == 1) {
-            this.goTo(this.userData.role[0])
-          }
-        } else {
-          if (res.data.user_disabled) {
-            this.user_disabled = true
-            localStorage.clear()
-            this.fullUserData = res.data.fullUserData
-            // console.log('this.fullUserData', this.fullUserData)
-          }
-        }
-      },
-      (error: any) => {
-        console.log('error here', error)
-      },
-    )
-  }
-
-
-
-  goTo(type: any) {
-    console.log("type", type)
-    this.httpService.playingRole = type
-    this.httpService.setItem('playingRole', type)
-    const request = {
-      method: 'GET',
-      action_url: 'user/get_user_module_permissions',
-    }
-    this.httpService.doHttp(request)?.subscribe(
-      (res: any) => {
-        if (res.status) {
-         // console.log("res of module permision", res.data)
-          this.httpService.menuData = res['data']
-          this.httpService.setItem('menuData', JSON.stringify(res['data']))
-          this.router.navigate(['user/dashboard'])
-        }
-      },
-      error => { },
-    )
-  }
 
 }
